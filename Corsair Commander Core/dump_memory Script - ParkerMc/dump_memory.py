6829d5efccc05ba2b8fe70514448372eb30fc325
ParkerMc <parkermc@parkermc.net>
2022-03-11 22:41:03 -0600

    commander core: support core xt

--- Corsair Commander Core/dump_memory Script - ParkerMc/dump_memory.py
+++ Corsair Commander Core/dump_memory Script - ParkerMc/dump_memory.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# Install hidapi
+# The purpose of this script is to dump the memery of the controller
+# When running the program it will first connect to the commander core
+# Then it go through all the "modes" and dump the data into dump.csv
 try:
     import hidraw as hid
 except ModuleNotFoundError:
@@ -10,7 +14,7 @@ _DUMP_MODES = [(0x07, 0x00), (0x17, 0x00), (0x18, 0x00), (0x1a, 0x00), (0x1e, 0x
                (0x63, 0x6d), (0x64, 0x6d), (0x65, 0x6d), (0x6a, 0x6d), (0x6b, 0x6d), (0x6c, 0x6d), (0x6d, 0x6d)]
 
 _VENDOR_ID = 0x1b1c
-_PRODUCT_ID = 0x0c1c
+_PRODUCT_IDS = [0x0c1c, 0x0c2a]
 _INTERFACE_NUMBER = 0
 
 _REPORT_LENGTH = 1024
@@ -23,12 +27,12 @@ _CMD_SET_MODE = (0x0d, 0x00)
 _CMD_GET = (0x08, 0x00)
 
 dev = None
-infos = hid.enumerate(_VENDOR_ID, _PRODUCT_ID)
+infos = hid.enumerate(_VENDOR_ID)
 for info in infos:
-    if info['interface_number'] == _INTERFACE_NUMBER:
+    if info['product_id'] in _PRODUCT_IDS and info['interface_number'] == _INTERFACE_NUMBER:
         dev = hid.device()
         dev.open_path(info['path'])
-        print('Found matching HID device')
+        print(f'Found matching HID device: {info["product_string"]}')
         break
 
 if dev is None:
